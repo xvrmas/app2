14493fa592f522dd1b395f0524016803040761dc
Xavier Mas <xvrmasgarcia@gmail.com>
2022-07-18 18:32:11 +0200

    primer commit

---
+++ src/components/Escena.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h4>{{ title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Escena",
+  props: ["title", "img"],
+  methods: {},
+};
+</script>
+
+<style>
+body {
+  display: grid;
+  align-content: center;
+  text-align: center;
+}
+h4 {
+  border: solid;
+  border-radius: 40px;
+  padding: 10px;
+}
+</style>
